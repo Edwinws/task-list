47b92067fb0f49edca53dee6e9e49846ed5a14e6
edwinws <william.edwinx@gmail.com>
2024-10-25 10:29:31 +0800

    frontend: initial commit, basic listing

---
+++ task-list-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import DataTable from '@/components/DataTable.vue';
+
+export default {
+  components: {
+    DataTable,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <DataTable />
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ task-list-frontend/src/components/DataTable.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <h1>Task List - Showing {{ currentPageTasks }} of {{ totalTasks }} tasks</h1>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Due Date</th>
+          <th>Status</th>
+          <th>Created At</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks" :key="task.id">
+          <td>{{ task.id }}</td>
+          <td>{{ task.name }}</td>
+          <td>{{ task.description }}</td>
+          <td>{{ task.dueDate }}</td>
+          <td>{{ task.status }}</td>
+          <td>{{ task.createdAt }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      tasks: [],
+      metas: {},
+      current: 0,
+      total: 0,
+    };
+  },
+  mounted() {
+    this.fetchData(); // Fetch data when component is mounted
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('http://localhost:3000/tasks');
+        this.tasks = response.data.data;
+        this.metas = response.data.meta;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
+  computed: {
+    currentPageTasks(): number {
+      return this.tasks.length;
+    },
+    totalTasks(): number {
+      return this.metas.total;
+    },
+  },
+}
+</script>
+
+<style scoped>
+table {
+  width: 200%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  padding: 8px 12px;
+  text-align: left;
+}
+</style>
