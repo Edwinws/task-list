2a52163110fe4ea57cb0394841566a5ce1345968
edwinws <william.edwinx@gmail.com>
2024-10-25 18:06:57 +0800

    backend: added findOne endpoint, moved status field check

--- task-list-backend/src/task/task.controller.ts
+++ task-list-backend/src/task/task.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskFindAllResponse } from './types/task-find-all.response';
+import { Task } from './types/task.model';
 
 @Controller('tasks')
 export class TaskController {
@@ -9,5 +16,17 @@ export class TaskController {
   @Get()
   async taskFindAll(): Promise<TaskFindAllResponse> {
     return this.taskService.findAll();
+  }
+
+  @Get(':id')
+  async taskFindOne(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    const task = await this.taskService.findOne(id);
+
+    if (task === null) {
+      // TODO: Should return a 404 error
+      throw new BadRequestException('Task not found');
+    }
+
+    return task;
   }
 }
--- task-list-backend/src/task/task.service.ts
+++ task-list-backend/src/task/task.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, Task } from '@prisma/client';
+import { Prisma, Task as TaskEntity } from '@prisma/client';
 import * as dayjs from 'dayjs';
 import { createPaginator } from 'prisma-pagination';
 import { PrismaService } from '../prisma.service';
 import { TaskFindAllResponse } from './types/task-find-all.response';
+import { Task } from './types/task.model';
 
 @Injectable()
 export class TaskService {
@@ -12,7 +13,7 @@ export class TaskService {
   async findAll(): Promise<TaskFindAllResponse> {
     const paginate = createPaginator({ page: 1 });
 
-    const result = await paginate<Task, Prisma.TaskFindManyArgs>(
+    const result = await paginate<TaskEntity, Prisma.TaskFindManyArgs>(
       this.prisma.task,
       {
         orderBy: {
@@ -22,27 +23,47 @@ export class TaskService {
     );
 
     const response: TaskFindAllResponse = { data: [], meta: result.meta };
-    const today = dayjs();
 
     for (const v of result.data) {
-      let status = '';
-      const dueDate = dayjs(v.dueDate);
-      const daysDifference = dueDate.diff(today, 'day');
-
-      if (daysDifference > 7) {
-        status = 'Not Urgent'; // TODO: Should be an enum
-      } else if (daysDifference >= 0 && daysDifference <= 7) {
-        status = 'Due soon';
-      } else {
-        status = 'Overdue';
-      }
-
       response.data.push({
-        status,
+        status: this.getTaskStatus(v.dueDate),
         ...v,
       });
     }
 
     return response;
+  }
+
+  async findOne(id: number): Promise<Task | null> {
+    const taskEntity = await this.prisma.task.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (taskEntity === null) {
+      return null;
+    }
+
+    const task: Task = {
+      status: this.getTaskStatus(taskEntity.dueDate),
+      ...taskEntity,
+    };
+
+    return task;
+  }
+
+  getTaskStatus(dueDate: Date): string {
+    const today = dayjs();
+    const dueDateDayjs = dayjs(dueDate);
+    const daysDifference = dueDateDayjs.diff(today, 'day');
+
+    if (daysDifference > 7) {
+      return 'Not Urgent'; // TODO: Should be an enum
+    } else if (daysDifference >= 0 && daysDifference <= 7) {
+      return 'Due soon';
+    } else {
+      return 'Overdue';
+    }
   }
 }
